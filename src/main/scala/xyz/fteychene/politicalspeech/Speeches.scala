b6485453fa66ef896f10bbcb7fb1d7dbf6c39018
fteychene <francois.teychene@gmail.com>
2020-03-06 16:15:58 +0100

    add new texts

--- src/main/scala/xyz/fteychene/politicalspeech/Speeches.scala
+++ src/main/scala/xyz/fteychene/politicalspeech/Speeches.scala
@@ -1,16 +1,12 @@
 package xyz.fteychene.politicalspeech
 
 import cats.Applicative
-import cats.implicits._
 import cats.effect.Sync
-import io.circe.{Decoder, Encoder}
+import cats.implicits._
 import io.circe.generic.semiauto.{deriveDecoder, deriveEncoder}
-import org.http4s.Method.GET
-import org.http4s.{EntityDecoder, EntityEncoder}
+import io.circe.{Decoder, Encoder}
 import org.http4s.circe.{jsonEncoderOf, jsonOf}
-import org.http4s.client.Client
-import org.http4s.client.dsl.Http4sClientDsl
-import xyz.fteychene.politicalspeech.Jokes.{Joke, JokeError}
+import org.http4s.{EntityDecoder, EntityEncoder}
 
 import scala.util.Random
 
@@ -18,6 +14,49 @@ trait Speeches[F[_]] {
 
   def get(): F[Speeches.Speech]
 
+}
+
+object SpeechesLines {
+
+  val first: List[String] = "Mesdames, messieurs," ::
+    "Je reste fondamentalement persuadé que" ::
+    "Dès lors, sachez que je me battrai pour faire admettre que" ::
+    "Par ailleurs c'est en toute connaissance de cause que je peux affirmer aujourd'hui que" ::
+    "je tiens a ous dire ici ma détermination sans faille pour clamer haut et fort que" ::
+    "J'ai depuis longtemps (ai-je besoin de vous le rappeler ?), défendu l'idée que" ::
+    "Et c'est en toute conscience que je déclare avec conviction que" ::
+    "Et ce n'est certainement pas vous, mes chers compatriotes, qui me contredirez si je vous dis que" ::
+    Nil
+
+  val second: List[String] = "la conjoncture actuelle" ::
+    "la situation d'exclusion que certain d'entre vous connaissent" ::
+    "l'acuité des problèmes de la vie quotidienne" ::
+    "la volonté farouche de sortir notre pays de la crise" ::
+    "l'effort prioritaire en faveur du statut précaire des exclus" ::
+    "le particularisme dû à notre histoire unique" ::
+    "l'aspiration plus ue légitime de chacun au progrès social" ::
+    "la nécessité de répondre à votre inquiétude journalière, que vous soyez jeune ou âgés," ::
+    Nil
+
+  val third: List[String] = "doit s'integrer à la finalisation globale" ::
+    "oblige à la prise en compte encore plus effective" ::
+    "interpelle le citoyen que je suis et nous oblige tous à aller de l'avant dans la voie" ::
+    "a pour conséquence obligatoire l'urgente nécessité" ::
+    "conforte mon désir incontestable d'aller dans le sens" ::
+    "doit nous amener au choxi réellement impératif" ::
+    "doit prendre en compte les préoccupations de la population de base dans l'élabiration" ::
+    "entraîne une mission somme toute des plus exaltantes pour moi : l'élaboration" ::
+    Nil
+
+  val fourth: List[String] = "d'un processus allant vers plus d'égalité" ::
+    "d'un avenir s'orientant vers plus de progres et plus de justice" ::
+    "d'une restructuration dans laquelle chacun pourra enfin retrouver sa dignité" ::
+    "dune valorisation sans concession de nos caractères spécifiques" ::
+    "d'un plan correspondant véritablement aux éxigences légitimes de chacun" ::
+    "de solutions rapides correspondant aux grands axes sociaux prioritaires" ::
+    "d'un programme plus humain, plus fraternel et plus juste" ::
+    "d'un projet porteur de véritables espoirs, notamment pour les plus démunis" ::
+    Nil
 }
 
 object Speeches {
@@ -42,12 +81,7 @@ object Speeches {
 
   def impl[F[_] : Sync](): Speeches[F] = new Speeches[F] {
 
-    val texts: List[List[String]] =
-      List("Mesdames, messieurs,", "Je reste fondamentalement persuadé que", "Dès lors, sachez que je me battrai pour faire admettre que") ::
-        List("la conjoncture actuelle", "la situation d'exclusion que certain d'entre vous connaissent", "l'acuité des problèmes de la vie quotidienne") ::
-        List("doit s'integrer à la finalisation globale", "oblige à la prise en compte encore plus effective", "interpelle le citoyen que je suis et nous oblige tous à aller de l'avant dans la voie") ::
-        List("d'un processus allant vers plus d'égalité", "d'un avenir s'orientant vers plus de progres et plus de justice", "d'une restructuration dans laquelle chacun pourra enfin retrouver sa dignité") ::
-        Nil
+    val texts: List[List[String]] = SpeechesLines.first :: SpeechesLines.second :: SpeechesLines.third :: SpeechesLines.fourth :: Nil
 
     def getRandomElement[A](seq: Seq[A])(implicit random: Random): A =
       seq(random.nextInt(seq.length))
